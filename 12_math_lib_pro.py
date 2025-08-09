d28b5cfad8587bf7eccb618da99fab8434f87ed1
prikshitbuilds <prikshitbuilds@gmail.com>
2025-08-09 19:08:19 +0530

    done all questins with numbers

---
+++ 12_math_lib_pro.py
@@ -0,0 +1,25 @@
+import math 
+
+print(math.floor(7.8))#7
+print(math.ceil(3.2)) #4
+
+#build in absoulte
+print(abs(-19))
+
+#math abs
+print(math.fabs(-19)) # float absolute
+
+
+# pi value in 4 places
+print(format(math.pi,".4f"))
+
+
+
+a = 5
+b = 2
+
+print(a / b) #2.5
+print(a // b) #2
+print(a % b) #1
+print(a ** b) #25
+print(int(5.9) + float(3)) #8.0
